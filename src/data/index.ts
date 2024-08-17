0d6adea382a7025e7a5a4052c83dd2f70f4f0765
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-08-16 21:57:48 -0600

    update: remove unused content from this file

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,7 +1,7 @@
-import type { NavigationItem } from "@/types";
+import type { NavigationItem, SectionItem, Skill } from "@/types";
 
-export const PAGE_TITLE = "Jorge Macias - Desarrollador de Software en Backend, Frontend y Aplicaciones de Escritorio";
-export const PAGE_DESCRIPTION = PAGE_TITLE;
+export const PAGE_TITLE = "Jorge Macias - Desarrollador de Software";
+export const PAGE_DESCRIPTION = "Jorge Macias - Desarrollador de Software en Backend, Frontend y Aplicaciones de Escritorio.";
 
 export const MENU_OPTIONS: NavigationItem[] = [
   { label: "Inicio", url: "/" },
