3c34c8f05f80c130d2c83280f5ee38c8c8242929
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-08-17 11:50:08 -0600

    upate: types for i18n json structure

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,4 +3,54 @@ export interface NavigationItem {
   url: string;
 }
 
+export interface SectionItem {
+  label: string;
+  url: string;
+}
+
+export interface Skill {
+  name: string;
+  icon: string;
+  description: string;
+}
+
+export interface Job {
+  companyName: string;
+  position: string;
+  description: string;
+  startDate: string;
+  finishDate: string;
+  linkedIn?: string;
+}
+
+export interface Project {
+  title: string;
+  description: string;
+  technologies: Technology[];
+  screenshots?: string[];
+  demoLink?: string;
+  roles: string[];
+  responsabilities: string[];
+  repoLink?: string;
+}
+
+export interface Technology {
+  name: string;
+  icon: string;
+}
+
+export interface Education {
+  degree: string;
+  institution: string;
+  campus: string;
+  startDate: string;
+  endDate: string;
+  gpa: string;
+  relevantProjects: string[];
+  courses: string[];
+  achievements: string[];
+  extracurricularActivities: string[];
+  schoolLink: string;
+}
+
 export type Theme = 'light' | 'dark'; 
