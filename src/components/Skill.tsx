f372537769f89f4de810b25f0cd925e5a2ab0348
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-08-17 16:09:05 -0600

    update(component): Skill

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,21 +1,29 @@
+import { $theme } from "@/stores/theme"
+import { useStore } from "@nanostores/react"
 
+interface SkillProps {
+  name: string
+  icon: string
+  description: string
+}
 
+export const Skill = ({ name, icon, description }: SkillProps) => {
+  const theme = useStore($theme);
 
+  const lightClassNames = "border bg-seasalt-500";
+  const darkClassNames = "border-raisin-black-600 bg-raisin-black";
 
-// interface SkillProps {
-//   name: string;
-//   iconName: string;
-//   description?: string;
-// }
+  const themeClassNames = theme === 'light' ? lightClassNames : darkClassNames;
 
-// export const Skill = ({ name, iconName, description }: SkillProps) => {
-//   return (
-//     <article className="border p-2">
-//       <h3 className="">{name} <span className={`${iconName} fa-dw`}></span> </h3>
-
-//       <p className="">
-//         {description}
-//       </p>
-//     </article>
-//   )
-// }
+  return (
+    <article className={`flex border flex-col rounded-md gap-2 p-2 items-center ${themeClassNames}`}>
+      <section>
+        <span className={`${icon} text-6xl text-center`}></span>
+      </section>
+      <section className="flex flex-col gap-2">
+        <h4 className="text-center font-bold text-2xl">{name}</h4>
+        <p>{description}</p>
+      </section>
+    </article>
+  )
+}
