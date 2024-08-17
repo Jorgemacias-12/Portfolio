019030335c8f8d36f4f1d4f7b4e7a191448499ae
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-08-16 21:54:04 -0600

    update: i18n types updated from json schema

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,5 +1,6 @@
 import english from '@/locales/en.json';
 import spanish from '@/locales/es.json';
+import type { Education, Job, Project, Skill } from '@/types';
 
 type LanguageType = {
   PAGE_TITLE: string;
@@ -15,6 +16,14 @@ type LanguageType = {
     label: string;
     url: string;
   }[];
+  SKILLS: {
+    LANGS: Skill[],
+    FRAMEWORKS: Skill[],
+    OTHERS: Skill[],
+  },
+  EXPERIENCES: Job[];
+  PROJECTS: Project[];
+  EDUCATION: Education[];
 };
 
 const LANGUAGES: Record<string, LanguageType> = {
