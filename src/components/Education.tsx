bd53033c4078ade45b250290aaf783d8a9c83a24
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-08-17 15:28:44 -0600

    add(component): Education
    Represents which grades or college I attend.

---
+++ src/components/Education.tsx
@@ -0,0 +1,38 @@
+import { type Education as EducationProps } from "@/types"
+import { Card } from "./Card"
+import { Details } from "./Details"
+
+export const Education = ({ degree, institution, campus, startDate, endDate, gpa, relevantProjects, courses, achievements, extracurricularActivities, schoolLink }: EducationProps) => {
+  return (
+    <Card aditionalClassNames="h-fit">
+      <h4 className="text-balance text-center font-bold text-lg">{degree}</h4>
+
+      <p className="text-md">{institution}</p>
+      <p className="text-md">{campus}</p>
+
+      <section className="flex gap-2 items-center">
+        <span className="fas fa-calendar"></span>
+
+        <div className="flex flex-wrap items-center gap-2">
+          <span className="rounded-full px-2 py-1 bg-indigo-300 text-indigo-800">{startDate}</span>
+          <span className="rounded-full px-2 py-1 bg-rose-300 text-rose-800">{endDate}</span>
+        </div>
+      </section>
+
+      <p className="flex gap-2 items-center bg-orange-300 rounded-full px-2 py-1 text-orange-800">
+        <span className="fas fa-scroll fa-dw"></span>
+        {gpa}
+      </p>
+
+      {relevantProjects && relevantProjects.length !== 0 && <Details title="Proyectos relevantes" content={relevantProjects} />}
+
+      {courses && courses.length !== 0 && <Details title="Especialidades" content={courses} />}
+
+      {achievements && achievements.length !== 0 && <Details title="Logros" content={achievements} />}
+
+      {extracurricularActivities && extracurricularActivities.length !== 0 && <Details title="Actividades extracurriculares" content={extracurricularActivities} />}
+
+      <a href={schoolLink} target="_blank" className="text-xs rounded-md bg-slate-900 border border-slate-600 text-slate-300 px-2 py-1">Ir a {institution}</a>
+    </Card>
+  )
+}
