d6a3a8898690a414315908d1a554570f579d9e10
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-08-18 04:01:33 -0600

    update: header layout and UX/UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,41 @@
-import { MENU_OPTIONS } from "@/data";
+import { getI18N } from "@/i18n";
 import { $theme } from "@/stores/theme";
 import { useStore } from "@nanostores/react";
 import { useEffect, useState } from "react";
 import { Link } from "./Link";
 import { ThemeSwitcher } from "./ThemeSwitcher";
-import { getI18N } from "@/i18n";
-
 
 interface HeaderProps {
   lang: string
 }
 
 export const Header = ({ lang }: HeaderProps) => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [useFixedHeader, setUseFixedHeader] = useState(false);
+  const theme = useStore($theme);
+
   const { COMPONENTS } = getI18N(lang);
   const { HEADER } = COMPONENTS;
 
-  const FIXED_SCROLL_THRESHOLD = 40;
-
-  const [showMenu, setShowMenu] = useState(false);
-  const theme = useStore($theme);
-  const [useFixedHeader, setUseFixedHeader] = useState(false);
+  const FIXED_SCROLL_THRESHOLD = 50;
 
   const headerDarkClassNames = "bg-raisin-black text-white border-none";
   const headerLightClassNames = "bg-seasalt text-black";
-  const menuBackground = theme === 'light' ? 'bg-white-smoke border divider-y' : 'bg-raisin-black';
-  const headerFixedClassNames = useFixedHeader ? `fixed w-full z-50 opacity-90` : "";
+  const headerFixedClassNames = useFixedHeader ? `fixed w-full z-50` : "";
+
+  const menuBackground = theme === 'light' ? 'bg-white-smoke md:bg-transparent' : 'bg-raisin-black md:bg-transparent';
+  const menuHiddenClasses = "pointer-events-none opacity-0 translate-x-[1rem]";
+  const menuShownClasses = "opacity-100 translate-x-0 pointer-events-auto";
+
+  const dividerLight = "";
+  const dividerDark = "border-raisin-black-600";
+  const borderColorClassName = theme === 'light' ? dividerLight : dividerDark;
 
   const handleMenuShow = () => {
     setShowMenu(previousValue => !previousValue);
   }
 
-  const menuHiddenClasses = "scale-80 translate-y-[-1rem] opacity-0 pointer-events-none";
-  const menuShownClasses = "scale-100 translate-y-0 opacity-100 pointer-events-auto";
-
-  const updateFixedHeaderState = () => {
+  const handleScroll = () => {
     const scrollOverpassed = window.scrollY > FIXED_SCROLL_THRESHOLD;
 
     setUseFixedHeader(scrollOverpassed);
@@ -48,37 +50,32 @@ export const Header = ({ lang }: HeaderProps) => {
   }
 
   useEffect(() => {
-    addEventListener('scroll', updateFixedHeaderState);
+    handleScroll();
+
+    addEventListener('scroll', handleScroll);
 
     return () => {
-      removeEventListener('scroll', updateFixedHeaderState);
+      removeEventListener('scroll', handleScroll);
     }
   }, [])
 
   return (
     <header className={`transition-all duration-300 border-b ${theme === 'light' ? headerLightClassNames : headerDarkClassNames} p-4 ${headerFixedClassNames}`}>
-      <section className="max-w-screen-xl mx-auto flex justify-between items-center">
-        <div className="flex items-center gap-2">
-          <h1 className="font-bold">Jorge Macias</h1>
-        </div>
-        <nav className="relative ">
-          <ul className="hidden lg:flex items-center gap-2">
-            {
-              MENU_OPTIONS.map(({ label, url }, index) => {
-                return <Link key={index} label={label} url={url} />
-              })
-            }
+      <section className="max-w-screen-xl mx-auto flex justify-between items-center gap-2">
+        <h1 className="font-bold md:text-2xl static z-20">Jorge Macias</h1>
 
-            <ThemeSwitcher />
-          </ul>
+        <nav className="flex flex-1 justify-end">
+          <button type="button" onClick={handleMenuShow} className={`fas fa-xl fa-fw ${showMenu ? 'fa-times' : 'fa-bars'} inline-flex cursor-pointer md:hidden`}></button>
 
-          <div className="flex gap-2 items-center lg:hidden">
-            <ThemeSwitcher />
-            <span onClick={handleMenuShow} className={`fas fa-xl fa-fw ${showMenu ? 'fa-times' : 'fa-bars'} inline-flex cursor-pointer`}></span>
-          </div>
+          <ul
+            className={`fixed top-0 right-0 w-fit h-full p-2 flex flex-col gap-1  ${menuBackground} z-10 tranisiton-all duration-300 transform ${showMenu ? menuShownClasses : menuHiddenClasses} md:static md:opacity-100  md:pointer-events-auto md:flex-row md:items-center`}
+          >
+            <section className={`border-b pb-1 flex items-center gap-2 justify-end ${borderColorClassName} md:border-0`}>
+              <ThemeSwitcher />
 
+              <button type="button" onClick={handleMenuShow} className={`my-5 fas fa-xl fa-fw ${showMenu ? 'fa-times' : 'fa-bars'} inline-flex cursor-pointer md:hidden`}></button>
+            </section>
 
-          <ul className={`absolute right-0 mt-5 p-2 rounded-md flex flex-col w-40 transition-all duration-300 z-50 ${menuBackground} ${showMenu ? menuShownClasses : menuHiddenClasses}`}>
             {
               HEADER.map(({ label, url }, index) => {
                 return <Link key={index} label={label} url={url}></Link>
