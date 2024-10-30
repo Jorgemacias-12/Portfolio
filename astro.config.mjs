4482334e5f7d63269fecce0aeb9bc1b84281267b
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2024-10-30 11:36:12 -0600

    chore(config): indicate base URL using .env
    BASE_URL env variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: "https://Jorgemacias-12.github.io",
+  base: import.meta.env.BASE_URL,
   integrations: [tailwind(), react()],
   i18n: {
     defaultLocale: 'es',
