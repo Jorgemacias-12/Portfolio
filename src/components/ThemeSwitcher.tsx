bbdbf86d3c068cc208720fc201d452fa98d8056d
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2024-12-03 07:24:43 -0600

    refact: use material icons components

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,20 +1,19 @@
-import { Switch } from './Switch'
-import { $theme } from '@/stores'
+import { Switch } from "./Switch";
+import { $theme } from "@/stores";
+
+import { LightMode, DarkMode } from "@mui/icons-material";
 
 export const ThemeSwitcher = () => {
-
   const onThemeChange = (state: boolean) => {
-    $theme.set(
-      $theme.get() === 'light' ? 'dark' : 'light'
-    )
-  }
+    $theme.set($theme.get() === "light" ? "dark" : "light");
+  };
 
   return (
-    <Switch 
-      defaultState={$theme.get() === 'light'} 
-      icons={["fa-sun", "fa-moon"]}
+    <Switch
+      defaultState={$theme.get() === "light"}
+      icons={[<DarkMode fontSize="small" />, <LightMode fontSize="small" />]}
       title="Theme switcher light/dark"
       onClick={onThemeChange}
     />
-  )
-}
+  );
+};
