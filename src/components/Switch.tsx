67de33fb1bb9e125d68548d7154f72d70e55ab0b
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2024-12-03 07:24:11 -0600

    refact: use material icon api

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,24 +1,24 @@
-import { $theme } from "@/stores"
-import { useStore } from "@nanostores/react"
-import { useState } from "react"
+import { $theme } from "@/stores";
+import { useStore } from "@nanostores/react";
+import { useState, type ReactNode } from "react";
 
-interface SwitchProps  {
-  icons?: [string | null, string | null] // falseIcon, trueIcon
-  title?: string
-  defaultState?: boolean
-  onClick?: (state: boolean) => void
+import Icon from "@mui/material/Icon";
+
+interface SwitchProps {
+  icons?: ReactNode[]; // falseIcon, trueIcon
+  title?: string;
+  defaultState?: boolean;
+  onClick?: (state: boolean) => void;
 }
 
-export const Switch = (
-  {
-    icons = ["fa-circle", "fa-circle"],
-    title = "Switch component",
-    defaultState = false,
-    onClick
-  }: SwitchProps
-) => {
+export const Switch = ({
+  icons = ["circle", "circle"],
+  title = "Switch component",
+  defaultState = false,
+  onClick,
+}: SwitchProps) => {
   const theme = useStore($theme);
-  const [state, setState] = useState(defaultState)
+  const [state, setState] = useState(defaultState);
 
   const toggleState = () => {
     setState(!state);
@@ -26,24 +26,29 @@ export const Switch = (
     if (onClick) {
       onClick(state);
     }
-  }
+  };
 
-  const commonIconClasses = "fas fa-md fa-fw";
-  const falseState = "translate-x-9";
-  const trueState = "translate-x-2"
+  const animationFalse = "translate-x-9";
+  const animationTrue = "translate-x-2";
   const buttonDarkBg = "border bg-whitesmoke-400";
   const buttonLightBg = "bg-whitesmoke-400 border";
 
-  const bg = theme === 'light' ? buttonLightBg : buttonDarkBg;
+  const bg = theme === "light" ? buttonLightBg : buttonDarkBg;
 
   return (
-    <button 
+    <button
       type="button"
       title={title}
       className={`inline-flex rounded-2xl w-16 relative h-8 ${bg}`}
       onClick={toggleState}
     >
-      <span className={`absolute left-0 top-1/2 transform -translate-y-1/2 transition-transform duration-300 ease-in-out ${commonIconClasses} ${state ? trueState : falseState} ${state ? icons[0] : icons[1]}`}></span>
+      <span
+        className={`absolute left-0 top-1/2 transform -translate-y-1/2 transition-transform duration-300 ease-in-out ${
+          state ? animationTrue : animationFalse
+        }`}
+      >
+        {state ? icons[0] : icons[1]}
+      </span>
     </button>
-  )
-}
+  );
+};
