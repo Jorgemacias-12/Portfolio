e0457fdb03728f1b662229fa995f8418a8d2e25e
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2024-12-05 16:06:14 -0600

    refactor: improve ux and adding transition to
    image movement

--- src/components/Carrousel.tsx
+++ src/components/Carrousel.tsx
@@ -1,49 +1,58 @@
+import { ChevronLeft, ChevronRight } from "@mui/icons-material";
 import { useState } from "react";
 
-interface CarrouselProps {
+interface Props {
   images: string[];
+  imageCaptions?: string[];
 }
 
-export const Carrousel = ({ images }: CarrouselProps) => {
-  const [image, setImage] = useState(0);
+export const Carrousel = ({ images, imageCaptions }: Props) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const onImageChangeNext = () => {
-    setImage((prevImage) => (prevImage + 1) % images!.length);
+  const handleNext = () => {
+    setCurrentIndex((prev) => (prev + 1) % images.length);
   };
 
-  const onImageChagePrev = () => {
-    setImage((prevImage) => {
-      if (prevImage === 0) {
-        return images!.length - 1;
-      } else {
-        return prevImage - 1;
-      }
-    });
+  const handlePrev = () => {
+    setCurrentIndex((prev) => (prev - 1 + images.length) % images.length);
   };
+
+  const buttonStyleClassNames =
+    "w-8 h-8 rounded-full flex items-center justify-center bg-black/50 text-white absolute";
 
   return (
-    <figure className="flex items-center gap-2 w-full justify-center relative">
+    <figure className="relative w-full h-auto overflow-hidden">
       <button
-        title="Change to previous image"
-        className="absolute top-1/2 left-2 w-8 h-8 rounded-full -translate-y-1/2 bg-black/50 text-white"
-        onClick={onImageChagePrev}
+        onClick={handlePrev}
+        className={`${buttonStyleClassNames} top-1/2 left-2 -translate-y-1/2 z-40`}
       >
-        <span className="fas fa-dw fa-angle-left"></span>
+        <ChevronLeft fontSize="large" />
       </button>
-      <section className="w-full">
-        <img
-          className="rounded-md w-full object-contain aspect-square"
-          src={images[image]}
-          alt=""
-        />
+      <button
+        onClick={handleNext}
+        className={`${buttonStyleClassNames} top-1/2 right-2 -translate-y-1/2 z-40`}
+      >
+        <ChevronRight fontSize="large" />
+      </button>
+      <section
+        className="relative flex transition-transform duration-500 ease-in-out"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        {images.map((url, index) => (
+          <img
+            key={index}
+            className="w-full h-auto object-cover flex-shrink-0"
+            src={url}
+            alt={imageCaptions?.[index] || `Image ${index + 1}`}
+          />
+        ))}
       </section>
-      <button
-        title="Change to next image"
-        className="absolute top-1/2 right-2 w-8 h-8 rounded-full -translate-y-1/2 bg-black/50 text-white"
-        onClick={onImageChangeNext}
-      >
-        <span className="fas fa-dw fa-angle-right"></span>
-      </button>
+
+      {imageCaptions && (
+        <figcaption className="absolute bottom-1 text- text-white bg-black/50 w-full p-2 left-1/2 -translate-x-1/2 z-40">
+          {imageCaptions?.[currentIndex]}
+        </figcaption>
+      )}
     </figure>
   );
 };
