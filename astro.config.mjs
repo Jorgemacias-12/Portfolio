7bcfc828f05fdfe3e0c0dc9b1b80593d869bc5f1
Jorgemacias-12 <jorge.macias7905@alumnos.udg.mx>
2025-05-09 03:17:13 -0600

    update: tailwind config file
    dark mode is now class based

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import { config } from 'dotenv';
-
-import min from 'astro-min';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import { config } from "dotenv";
 
 config();
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://Jorgemacias-12.github.io",
-  base: `${process.env.PUBLIC_BASE_URL || ''}`,
+  base: `${process.env.PUBLIC_BASE_URL || ""}`,
   integrations: [tailwind(), react()],
   i18n: {
-    defaultLocale: 'es',
-    locales: ['es', 'en'],
+    defaultLocale: "es",
+    locales: ["es", "en"],
     routing: {
       prefixDefaultLocale: false,
-      strategy: 'pathname'
-    }
-  }
+      strategy: "pathname",
+    },
+  },
 });
