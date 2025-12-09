bd587d06bcb399acd3b1f37f2fe7a47bdf0b9d74
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2025-12-08 21:28:24 -0600

    update: astro.config.mjs
    add:
    
     - site
     - base
     - i18n object

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: "https://jorgemacias.dev",
+  base: process.env.PUBLIC_BASE_URL || "",
+  i18n: {
+    defaultLocale: "es",
+    locales: ["en", "es"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
