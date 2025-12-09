1823e7aa5a5b00010fd2695ab67f39c817c435a2
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2025-12-08 19:04:05 -0600

    update: REMOVE PROJECT
    Ah yes, rework, with the things I learned from other projects.

--- src/stores/index.ts
+++
@@ -1,8 +0,0 @@
-import type { Theme } from "@/types";
-import { getThemeBasedOnUserPreference } from "@/utils";
-import { persistentAtom } from "@nanostores/persistent";
-
-export const $theme = persistentAtom<Theme>(
-  "theme",
-  getThemeBasedOnUserPreference()
-);
