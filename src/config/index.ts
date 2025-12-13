f1bef67865fc1330a4382327327d87ef0e2a367f
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2025-12-13 05:19:33 -0600

    add: src/config/index.ts
    contains constants that allows to centralize the information, or in this
    case the configuration of which components has to import to get the job
    done.

---
+++ src/config/index.ts
@@ -0,0 +1,14 @@
+import type { AstroComponent } from "@/types";
+
+export const ICON_SIZE_IN_PX = 20;
+
+export const SECTION_COMPONENTS: Record<
+  string,
+  () => Promise<{ default: AstroComponent }>
+> = {
+  skills: () => import("@/sections/Skills.astro"),
+  about: () => import("@/sections/About.astro"),
+  experience: () => import("@/sections/Experiences.astro"),
+  projects: () => import("@/sections/Projects.astro"),
+  education: () => import("@/sections/Education.astro"),
+};
