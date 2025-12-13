d109396b2883d1468493bcb542074368958e81a6
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2025-12-13 11:43:57 -0600

    update(ci): install astro sitemap
    with the configuration applied

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,16 +6,13 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import min from "astro-min";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    maintenance({
-      enabled: process.env.MAINTENANCE_ENABLED === "true",
-    }),
-    react(),
-    min(),
-  ],
+  integrations: [tailwind(), maintenance({
+    enabled: process.env.MAINTENANCE_ENABLED === "true",
+  }), react(), min(), sitemap()],
   site: "https://jorgemacias.dev",
   base: process.env.PUBLIC_BASE_URL || "",
   i18n: {
