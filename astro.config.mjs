927c1b699a9afcf5cf741b2bd68f69d71e7115d1
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2025-12-12 22:18:55 -0600

    update: astro.config.mjs
    update astro configuration file to use this integrations:
    
    - react
    - min (minfy action)
    - maintenance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import maintenance from "astro-maintenance";
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import min from "astro-min";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    maintenance({
+      enabled: process.env.MAINTENANCE_ENABLED === "true",
+    }),
+    react(),
+    min(),
+  ],
   site: "https://jorgemacias.dev",
   base: process.env.PUBLIC_BASE_URL || "",
   i18n: {
