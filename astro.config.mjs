0bc0b6815d2c5aeeae6ea43e884a754893cd42a8
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2025-12-16 12:47:07 -0600

    udpate: package.json
    and astro config file
    
    install:
    
     - purgecss and use it in the config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,17 +7,13 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 
+import purgecss from "astro-purgecss";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    maintenance({
-      enabled: process.env.MAINTENANCE_ENABLED === "true",
-    }),
-    react(),
-    sitemap({}),
-    compress(),
-  ],
+  integrations: [tailwind(), maintenance({
+    enabled: process.env.MAINTENANCE_ENABLED === "true",
+  }), react(), sitemap({}), compress(), purgecss()],
   site: "https://jorgemacias.dev",
   base: process.env.PUBLIC_BASE_URL || "",
   i18n: {
