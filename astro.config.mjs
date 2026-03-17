3b66339486668746ab8a7c62ac0179251649c826
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2026-03-16 21:10:00 -0600

    update: astro configuration file
    Add site, base, i18n config and also add intregration "astro-compress"
    and obiously install it.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://www.jorgemacias.dev/",
+  base: process.env.PUBLIC_BASE_URL || "",
+
+  i18n: {
+    defaultLocale: "es",
+    locales: ["en", "es"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+
+  integrations: [compress()],
+});
