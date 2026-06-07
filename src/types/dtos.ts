e6e6ee0335a847d90bb83a04ce16084e0ad6616f
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2026-06-07 01:40:11 -0600

    update: dtos.ts
    Add the following types
    
    LinkType
    EducationStatus
    
    from the Icon interface remove isLucideIcon prop
    
    add the following interfaces
    
    - Image
    - Video
    - Link
    - VerificationLink
    - Education
    - Project
    - Point
    - SocialLink

--- src/types/dtos.ts
+++ src/types/dtos.ts
@@ -1,8 +1,10 @@
+export type LinkType = "social" | "oficial";
+export type EducationStatus = "graduated" | "completed" | "ongoing";
+
 export interface Icon {
   name: string;
   size?: number;
   class?: string;
-  isLucideIcon?: boolean;
 }
 
 export interface SocialNetwork {
@@ -29,4 +31,63 @@ export interface Job {
   period: string;
   responsibilities: string[];
   technologies: TechnicalSkill[];
+}
+
+export interface Image {
+  url: string;
+  description: string;
+}
+
+export interface Video {
+  url: string;
+  settings: object;
+}
+
+export interface Link {
+  label: string;
+  href: string;
+  icon?: string;
+}
+
+export interface VerificationLink extends Link {
+  type: LinkType;
+}
+
+export interface Education {
+  id: string;
+  degree: string;
+  institution: string;
+  campus: string;
+  location: string;
+  startDate: string;
+  endDate: string;
+  gpa: string;
+  status: EducationStatus;
+  finished: boolean;
+  schoolLink: string;
+  schoolIconName: string;
+  verificationLinks: VerificationLink[];
+  relevantProjects: Link[];
+  achievements: string[];
+}
+
+export interface Project {
+  name: string;
+  description: string;
+  media?: Image[];
+  demo_link?: string;
+  repo_link?: string;
+  links?: Link[];
+}
+
+export interface Point {
+  x: number;
+  y: number;
+}
+
+export interface SocialLink {
+  url: string;
+  label: string;
+  description: string;
+  icon?: Icon;
 }
